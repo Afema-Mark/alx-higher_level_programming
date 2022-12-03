618545c5d70a6ab7ae180efa90edfbf0e85bc075
Afema-Mark <afemamark@gmail.com>
2022-12-03 18:46:13 +0000

    “task6”

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         l = 1
-            for j in i:
+        for j in i:
             if l == len(i):
                 print("{:d}".format(j), end="")
             else:
